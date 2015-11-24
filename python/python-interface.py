98e01a1e37dc6d5c5a0633947da4ad3158c517c9
MotherBrain <aespielberg@ubuntu.(none)>
2015-11-24 08:38:06 -0800

    Updated python code to be a utility class rather than a stupid script

--- python/python-interface.py
+++ python/python-interface.py
@@ -1,14 +1,43 @@
 #!/usr/bin/python
 
 from websocket import create_connection
-import IPython
+from threading import Thread
 
-ws = create_connection("ws://lickr.herokuapp.com:80")
+class LickrListener:
 
-ws.send("machine")
-print "Sent"
-print "Receiving..."
-while True:
-    result =  ws.recv()
-    print "Received '%s'" % result
-ws.close()
+    ws = None
+    queue = []
+    
+    def create_connection(self, uri):
+        self.ws = create_connection(uri)
+        self.ws.send("machine")
+   
+
+    def run(self):
+
+        while True:
+            result = self.ws.recv()
+            result = result.split(',')
+            result = [ int(x) for x in result ]
+            self.queue.append(result)
+    
+    def close(self):    
+        self.ws.close()
+        
+    def pop_queue(self):
+        if self.queue: #if the queue is not empty:
+            return self.queue.pop(0) #TODO: very inefficient, maybe I should consider a different datastructure
+        else:
+            return None #nothing to return!
+
+if __name__ == "__main__":
+    #TODO: main method here
+    ll = LickrListener()
+    ll.create_connection("ws://lickr.herokuapp.com:80")
+    thread = Thread(target = ll.run)
+    thread.start()
+    
+    while True:
+        popped = ll.pop_queue()
+        if popped: #if it's not None
+            print popped
