4baf430d5df3b35e808a06b272659b476283ef36
MotherBrain <aespielberg@ubuntu.(none)>
2015-12-05 16:03:55 -0800

    integration of Jasmin's code

--- python/python-interface.py
+++ python/python-interface.py
@@ -2,11 +2,23 @@
 
 from websocket import create_connection
 from threading import Thread
+from numpy import sqrt
 
 class LickrListener:
 
     ws = None
     queue = []
+    
+    # length of arms
+    L = 10
+
+    # Delta Radius - distance from edge of end effector to point under center of carriage
+    DR = 10
+
+    # distance the head extends below the effector (Length of tongue)
+    Hcz = 5
+    
+    jog_height = 10
     
     def __init__(self):
         pass
@@ -25,6 +37,8 @@ class LickrListener:
             result = self.ws.recv()
             result = result.split(',')
             result = [ int(x) for x in result ]
+            #convert it:
+            result = self.translate_coordinates(result)
             self.queue.append(result)
     
     def close(self):    
@@ -39,6 +53,25 @@ class LickrListener:
     def run_in_background(self):
         thread = Thread(target = ll.run)
         thread.start()
+        
+    def translate_coordinates(self, coords):
+    #Translates the coordinates from [x, y, z] to [A, B, C]
+
+        X = coords[0]
+        Y = coords[1]
+        Z = coords[2] * self.jog_height #coords[2] is 1 or 0, so this is jog_height or 0
+        
+        L = self.L
+        DR = self.DR
+        Hcz = self.Hcz
+
+        # A is the rode on the Y axis, B is 120 degrees clockwise from A, C is 120 degrees clockwise from 
+        # X, Y ,Z are the input poin recieved from the user touch on the screen
+        Az = sqrt(pow(L, 2) - pow((X - 0), 2) - pow((Y - DR), 2)) + Z + Hcz
+        Bz = sqrt(pow(L, 2) - pow((X - DR*sqrt(3)/2), 2) - pow((Y + DR/2),2)) + Z + Hcz
+        Cz = sqrt(pow(L, 2) - pow((X + DR*sqrt(3)/2), 2) - pow((Y + DR/2),2)) + Z + Hcz
+        
+        return [Az, Bz, Cz]
 
 if __name__ == "__main__":
     #TODO: main method here
