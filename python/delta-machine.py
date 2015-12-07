b6d07bd6aca6c5a7a7482beb4d9b2947b8272f3a
MotherBrain <aespielberg@ubuntu.(none)>
2015-12-07 15:17:12 -0800

    Started creating delta-machine class

---
+++ python/delta-machine.py
@@ -0,0 +1,63 @@
+#------IMPORTS-------
+from pygestalt import nodes
+from pygestalt import interfaces
+from pygestalt import machines
+from pygestalt import functions
+from pygestalt.machines import elements
+from pygestalt.machines import kinematics
+from pygestalt.machines import state
+from pygestalt.utilities import notice
+from pygestalt.publish import rpc	#remote procedure call dispatcher
+import time
+import io
+
+
+#------VIRTUAL MACHINE------
+class virtualMachine(machines.virtualMachine):
+	
+	def initInterfaces(self):
+		if self.providedInterface: self.fabnet = self.providedInterface		#providedInterface is defined in the virtualMachine class.
+		else: self.fabnet = interfaces.gestaltInterface('FABNET', interfaces.serialInterface(baudRate = 115200, interfaceType = 'ftdi', portName = '/dev/ttyUSB0'))
+		
+	def initControllers(self):
+		self.aAxisNode = nodes.networkedGestaltNode('A Axis', self.fabnet, filename = '086-005a.py', persistence = self.persistence)
+		self.bAxisNode = nodes.networkedGestaltNode('B Axis', self.fabnet, filename = '086-005a.py', persistence = self.persistence)
+		self.cAxisNode = nodes.networkedGestaltNode('C Axis', self.fabnet, filename = '086-005a.py', persistence = self.persistence)
+
+		self.abcNode = nodes.compoundNode(self.aAxisNode, self.bAxisNode, self.cAxisNode)
+
+	def initCoordinates(self):
+		self.position = state.coordinate(['mm', 'mm', 'mm'])
+	
+	def initKinematics(self):
+		#TODO: is this correct?
+		self.aAxis = elements.elementChain.forward([elements.microstep.forward(4), elements.stepper.forward(1.8), elements.leadscrew.forward(8), elements.invert.forward(True)])
+		self.bAxis = elements.elementChain.forward([elements.microstep.forward(4), elements.stepper.forward(1.8), elements.leadscrew.forward(8), elements.invert.forward(False)])
+		self.cAxis = elements.elementChain.forward([elements.microstep.forward(4), elements.stepper.forward(1.8), elements.leadscrew.forward(8), elements.invert.forward(False)])
+				
+		self.stageKinematics = kinematics.direct(3)	#direct drive on all axes
+	
+	def initFunctions(self):
+		self.move = functions.move(virtualMachine = self, virtualNode = self.xyNode, axes = [self.aAxis, self.bAxis, self.cAxis], kinematics = self.stageKinematics, machinePosition = self.position,planner = 'null')
+		self.jog = functions.jog(self.move)	#an incremental wrapper for the move function
+		pass
+		
+	def initLast(self):
+		#self.machineControl.setMotorCurrents(aCurrent = 0.8, bCurrent = 0.8, cCurrent = 0.8)
+		#self.xNode.setVelocityRequest(0)	#clear velocity on nodes. Eventually this will be put in the motion planner on initialization to match state.
+		pass
+	
+	def publish(self):
+		#self.publisher.addNodes(self.machineControl)
+		pass
+	
+	def getPosition(self):
+		return {'position':self.position.future()}
+	
+	def setPosition(self, position  = [None]):
+		self.position.future.set(position)
+
+	def setSpindleSpeed(self, speedFraction):
+		#self.machineControl.pwmRequest(speedFraction)
+		pass
+
